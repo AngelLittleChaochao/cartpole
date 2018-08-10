e0d11f3b7426b4d81e0273c987d3d21935b650d7
chaochao <chtang@microsoft.com>
2018-08-10 21:00:26 +0800

    change state to int

--- main.py
+++ main.py
@@ -6,28 +6,87 @@ env = gym.make('CartPole-v0')
 def toint(f):
     return round(f*1000)
 
+# --------------------------------------------
+# Get unique state value for observed state.
+# @para x: x position
+# --------------------------------------------
+def get_state(x, x_dot, theta, theta_dot):
+    one_degree = 0.0174532
+    six_degrees = 0.1047192
+    twelve_degrees = 0.2094384
+    fifty_degrees = 0.87266
+    
+    if x < -2.4 or x > 2.4 or theta < -0.2094384 or theta > 0.2094384:
+        return -1
+    state = 0
+
+    if x < -0.8:
+        state = 0
+    elif x < 0.8:
+        state = 1
+    else:
+        state = 2
+
+    if x_dot < -0.5:
+        state = state
+    elif x_dot < 0.5:
+        state += 3
+    else:
+        state += 6
+
+    if theta < -six_degrees:
+        state = state
+    elif theta < -one_degree:
+        state += 9
+    elif theta < 0:
+        state += 18
+    elif theta < one_degree:
+        state += 27
+    elif theta < six_degrees:
+        state += 36
+    else:
+        state += 45
+
+    if theta_dot < -fifty_degrees:
+        state = state
+    elif theta_dot < fifty_degrees:
+        state += 54
+    else:
+        state += 108
+
+    return state    
+
 Q = dict()
-gamma = 0.6
-for i_episode in range(100):
+gamma = 0.9
+prev_state = -1
+prev_action = -1
+for i_episode in range(1000):
     observation = env.reset()
     for t in range(100):
         env.render()
-        #print(observation)
-        state = toint(observation[2])
+        #print(observation)        
+        state = get_state(observation[0], observation[1], observation[2], observation[3])
+
+        -- update prev_state Q value
+        if prev_state ~= -1 and prev_action ~= -1:
+            if prev_state not in Q:
+                Q[prev_state] = [0,0]
+            if state not in Q:
+                Q[state] = [0,0]
+            Q[prev_state][action] = reward + gamma * next_max
+        
+        
         r = random.randint(0, 9)
-        if state in Q and r <= 4:
+        if state in Q and r <= 6:
             if Q[state][0] > Q[state][1]:
                 action = 0
             else:
                 action = 1
         else:
-            if state < 0:
-                action = 0
-            else:
-                action = 1                    
+            action = random.randint(0, 1)            
                 
         observation, reward, done, info = env.step(action)
-        next_state = toint(observation[2])
+        next_state = get_state(observation[0], observation[1], observation[2], observation[3])
 
         next_max = 0
         if next_state in Q:
@@ -39,7 +98,7 @@ for i_episode in range(100):
         if state not in Q:
             Q[state] = [0, 0]        
             
-        Q[state][action] = reward + gamma * next_max                
+        
                 
         if done:
             #print(Q)
