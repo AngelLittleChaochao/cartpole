3afd93acef26bf5a07cd21c997b47f7c54d7397c
chaochao <chtang@microsoft.com>
2018-08-17 16:33:39 +0800

    add script for restarting/build docker

---
+++ container_run.sh
@@ -0,0 +1,4 @@
+docker run --name tensorflow \
+       -it \
+       -v $PWD/../cartpole:/usr/src/cartpole \
+ cdev /bin/bash
---
+++ restart.sh
@@ -0,0 +1,1 @@
+docker stop -t 0 tensorflow; docker rm tensorflow; ./container_run.sh
